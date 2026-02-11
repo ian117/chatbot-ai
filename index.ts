2362bbeaba1024e7702eaf3bf222847aaf670834
ian117 <spideramerica71@gmail.com>
2026-02-10 23:49:29 -0600

    ci: Concurrently start the project in dev mode

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+    {
+        name: "server",
+        command: "bun run dev",
+        prefixColor: "cyan",
+        cwd: "packages/server",
+    },
+    {
+        name: "client",
+        command: "bun run dev",
+        prefixColor: "yellow",
+        cwd: "packages/client",
+    },
+])
